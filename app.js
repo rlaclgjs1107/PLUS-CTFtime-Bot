33be704b0717cac5c7a0bcfa5490a1dbf1fcce0f
rlaclgjs1107 <rlaclgjs1107@gmail.com>
2020-10-02 03:40:18 +0900

    😴

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.json');
 
 const getCTFTimeEvents = require('./crawl.js');
+const { stringify } = require('querystring');
+
+let eventFileName = "./ctfTimeEvents.json";
+let encoding = "utf8";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -13,18 +19,38 @@ client.on('message', async msg => {
     if (content.slice(0,2) == './') {
         content = content.slice(2);
         if(content == 'help') {
-            msg.reply("plus-ctftime-bot")
+            msg.reply("\n./ping : pong \n ./list [n] : list n ctfs (max : 10 / default : 5) \n ./update : update ctf list\n");
         } else if(content == 'ping') {
             msg.reply("pong");
-        } else if(content == 'list') {
-            let ctfTitles = "";
-            let ctfEvents = await getCTFTimeEvents();
-            ctfEvents.forEach(function(elem, idx){
-                ctfTitles = ctfTitles.concat("[",idx,"] ",elem.title, "\n");
+        } else if(content.startsWith('list')) {
+            let num = parseInt(content.slice(4));
+            if(isNaN(num) || num==0) num = 5; 
+            if(num > 10) num = 10;
+            fs.readFile(eventFileName, encoding, function(err, data) {
+                if(err) {
+                    msg.reply("You should update first!");
+                } else {         
+                    let res = "\n";
+                    let ctfTimeEvents = [];
+                    let data_ = data.slice(2,-2).split("},{");
+                    data_.forEach(function(elem,i){
+                        ctfTimeEvents[i] = JSON.parse("{"+elem+"}");
+                    });
+                    for(var i = 0; i < num; i++){
+                        res = res.concat("[",i+1,"] Title : ",ctfTimeEvents[i].title,"\n");
+                        res = res.concat("[+] Date : ", ctfTimeEvents[i].date, "\n");
+                        res = res.concat("[+] Weight : ", ctfTimeEvents[i].weight, "\n\n");
+                    }
+                    msg.reply(res);
+                }
             });
-            msg.reply(ctfTitles);
         } else if(content == 'update') {
-            
+            let ctfEvents = await getCTFTimeEvents(); 
+            let data = JSON.stringify(ctfEvents);
+            fs.writeFile(eventFileName, data, encoding, function(err){
+                if(err) console.log(err);
+                else msg.reply("List Updated Successfully!");
+            })
         }
     }
 });
--- crawl.js
+++ crawl.js
@@ -10,14 +10,22 @@ async function getHTML() {
 }
 
 async function getCTFTimeEvents() {
+    let idx = 0;
     let ctfTimeEvents = [];
     let html = await getHTML();
     const $ = cheerio.load(html.data);
     const ctf = $("table.table-striped tbody").children("tr");
     ctf.each(function(i,elem) {
-        ctfTimeEvents[i] = {
-            title : $(this).find("td a").text()
-        };
+        if($(this).find("td a").text()!=""){
+            let detail = $(this).html().replace(/<\/td>/gi, "").split("<td>");
+            ctfTimeEvents[idx] = {
+                title : $(this).find("td a").text(),
+                date : detail[2].replace(/&#x2014;/gi, "-"),
+                format : detail[3],
+                weight : detail[5]
+            };
+            idx += 1;
+        }
     });
     return ctfTimeEvents;
 }
