9f4109a0c40f800de8cc3829df682ac0c1f79f17
rlaclgjs1107 <rlaclgjs1107@gmail.com>
2020-10-02 00:59:01 +0900

    app.js

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.json');
 
+const getCTFTimeEvents = require('./crawl.js');
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content == 'ping') {
-        msg.reply('pong');
+    let content = msg.content;
+    if (content.slice(0,2) == './') {
+        content = content.slice(2);
+        if(content == 'help') {
+            msg.reply("plus-ctftime-bot")
+        } else if(content == 'ping') {
+            msg.reply("pong");
+        } else if(content == 'list') {
+            const ctfEvents = getCTFTimeEvents();
+            console.log(ctfEvents);
+            msg.reply(ctfEvents);
+        } else if(content == 'update') {
+
+        }
     }
 });
 
