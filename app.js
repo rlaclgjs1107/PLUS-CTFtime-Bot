2a84aa56b802058a78eacb76bba27643515a8438
rlaclgjs1107 <rlaclgjs1107@gmail.com>
2020-11-03 02:01:20 +0900

    using APIs

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.json');
+const axios = require('axios');
+const { response } = require('express');
 
-const getCTFTimeEvents = require('./crawl.js');
-const { stringify } = require('querystring');
-
-let eventFileName = "./ctfTimeEvents.json";
-let encoding = "utf8";
+const CTAPI_EVENT_URL = "https://ctftime.org/api/v1/events"
+const CTAPI_REQUEST_HEADERS = {
+    "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -19,39 +20,42 @@ client.on('message', async msg => {
     if (content.slice(0,2) == './') {
         content = content.slice(2);
         if(content == 'help') {
-            msg.reply("\n./ping : pong \n ./list [n] : list n ctfs (max : 10 / default : 5) \n ./update : update ctf list\n");
+            msg.reply("\n./ping : pong \n ./list: list ctfs in this week (limit = 10)\n ./info {event_id} : get more information of the event\n");
         } else if(content == 'ping') {
             msg.reply("pong");
         } else if(content.startsWith('list')) {
-            let num = parseInt(content.slice(4));
-            if(isNaN(num) || num==0) num = 5; 
-            if(num > 10) num = 10;
-            fs.readFile(eventFileName, encoding, function(err, data) {
-                if(err) {
-                    msg.reply("You should update first!");
-                } else {         
-                    let res = "\n";
-                    let ctfTimeEvents = [];
-                    let data_ = data.slice(2,-2).split("},{");
-                    data_.forEach(function(elem,i){
-                        ctfTimeEvents[i] = JSON.parse("{"+elem+"}");
-                    });
-                    for(var i = 0; i < num; i++){
-                        res = res.concat("[",i+1,"] Title : ",ctfTimeEvents[i].title,"\n");
-                        res = res.concat("[+] Date : ", ctfTimeEvents[i].date, "\n");
-                        res = res.concat("[+] Weight : ", ctfTimeEvents[i].weight, "\n\n");
+            let res = "\n"
+            let start = new Date().getTime();
+            let finish = start + 604800000;
+            axios.get(`${CTAPI_EVENT_URL}/?limit=10&start=${start}&finish=${finish}`,{headers:CTAPI_REQUEST_HEADERS})
+                .then(response => {
+                    for(var i=0; i<10; i++){
+                        let event = response.data[i];
+                        res = res.concat("[",i,"] Title : ",event.title,"\n");
+                        res = res.concat("[+] Date  : ", event.start, " ~ ", event.finish, "\n");
+                        res = res.concat("[+] Event Id : ", event.id, "\n\n");
                     }
                     msg.reply(res);
-                }
-            });
-        } else if(content == 'update') {
-            let ctfEvents = await getCTFTimeEvents(); 
-            let data = JSON.stringify(ctfEvents);
-            fs.writeFile(eventFileName, data, encoding, function(err){
-                if(err) console.log(err);
-                else msg.reply("List Updated Successfully!");
-            })
-        } else if(content == 'cat flag') {
+                })
+                .catch(error => msg.reply("Something Wrong....\n"))
+        } else if(content.startsWith('info')) {
+            let id = parseInt(content.slice(4));
+            let res = "\n"
+            if(!isNaN(id)){
+                axios.get(`${CTAPI_EVENT_URL}/${id}/`,{headers:CTAPI_REQUEST_HEADERS})
+                    .then(response => {
+                        let event = response.data;
+                        res = res.concat("[-] Title : ",event.title,"\n");
+                        res = res.concat("[+] Date  : ",event.start, " ~ ", event.finish, "\n");
+                        res = res.concat("[+] Format : ",event.format,"\n");
+                        res = res.concat("[+] Weight : ",event.weight,"\n");
+                        res = res.concat("[+] URL : ",event.url,"\n\n");
+                        msg.reply(res);
+                    })
+                    .catch(error => msg.reply("Something Wrong....\n"))
+            } else {msg.reply("Invalid id\n");}
+        } 
+        else if(content == 'cat flag') {
             msg.reply("PLUS{D0_y0u_l0v3_c47?_1_l0v3_c47_t000!\\_:>}");
         }
     }
